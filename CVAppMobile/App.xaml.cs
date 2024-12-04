c15d1fb42c5771a9dbfd90c88d024b3c63c6bdcd
djuareza <djuareza@sigma-alimentos.com>
2024-12-03 19:39:41 -0600

    Proyecto
    Se crea el proyecto

---
+++ CVAppMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CVAppMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CVAppMobile/Platforms/Tizen/Main.cs
@@ -0,0 +1,17 @@
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using System;
+
+namespace CVAppMobile
+{
+    internal class Program : MauiApplication
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            app.Run(args);
+        }
+    }
+}
